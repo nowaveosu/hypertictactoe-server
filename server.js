a5e6a51c8a87f54c816ec00e1e4778458032945e
nowaveosu <nowaveosu@gmail.com>
2024-05-24 21:13:36 +0900

    [Add] origin 변경

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(3000, {
     cors: {
-        origin: "http://localhost:3001",
+        origin: ["https://hyper-tictactoe.com", "https://www.hyper-tictactoe.com"],
         methods: ["GET", "POST"],
     },
 });
